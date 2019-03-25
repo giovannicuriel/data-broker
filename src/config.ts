2d33b9b818eb79237229667a8a8ad0ff4502accf
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2019-03-25 11:40:40 -0300

    feat: adding healthcheck

--- src/config.ts
+++ src/config.ts
@@ -26,4 +26,15 @@ const cache = {
   user: process.env.DATABROKER_CACHE_USER || "redis",
 };
 
-export { kafka, broker, cache };
+const healthcheck = {
+  timeout: {
+    cpu: Number(process.env.HC_CPU_USAGE_TIMEOUT) || 300000,
+    kafka: Number(process.env.HC_KAFKA_TIMEOUT) || 30000,
+    memory: Number(process.env.HC_MEMORY_USAGE_TIMEOUT) || 300000,
+    mongodb: Number(process.env.HC_MONGODB_TIMEOUT) || 30000,
+    uptime: Number(process.env.HC_UPTIME_TIMEOUT) || 300000,
+  }
+};
+
+
+export { kafka, broker, cache, healthcheck};
